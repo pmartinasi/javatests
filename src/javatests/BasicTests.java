48770644f50385cb3d8eb03d8909a3487a1c9668
Pedro M. <pmartinasi@gmail.com>
2025-05-01 00:01:41 +0200

    20250430 - Basic Test

---
+++ src/javatests/BasicTests.java
@@ -0,0 +1,14 @@
+package javatests;
+
+public class BasicTests {
+
+	public BasicTests() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
